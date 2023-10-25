6a8493f6ad0b4cff55f8e40564ca3735cb364965
William Kaiser <wkaisertexas@gmail.com>
2023-10-25 13:45:52 -0400

    Initial Commit

---
+++ TestPRoject/TestPRojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestPRojectApp.swift
+//  TestPRoject
+//
+//  Created by William Kaiser on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct TestPRojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
